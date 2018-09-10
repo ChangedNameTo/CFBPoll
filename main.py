c448964c4c7c2af24072f6da09dc61d28c7d8ff3
ChangedNameTo <william.m95@gmail.com>
2018-09-10 00:50:49 -0400

    Markdown table fix

--- main.py
+++ main.py
@@ -507,6 +507,7 @@ def season_output(week, year):
     with open("season_evolution.txt", "w") as file:
         # Writes the table header
         file.write("|Week\Rank|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|\n")
+        file.write("|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|---|\n")
 
         # Creates the flair map for everyone
         flair_map = generate_flair_map()
