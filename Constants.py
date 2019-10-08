d5d98f9a13a3df508d6b24a9b4eee909ecd12683
Will Mitchell <william.m95@gmail.com>
2019-10-08 12:56:39 -0400

    Prediction date fix

--- Constants.py
+++ Constants.py
@@ -6,7 +6,6 @@ SCHEDULE_URL = 'http://prwolfe.bol.ucla.edu/cfootball/schedules.htm'
 YEAR      = 2019
 WEEK      = 7
 K_VALUE   = (30 - WEEK)
-MEAN_REV  = min((0.1 * WEEK, 1))
 
-START_DATE = '03-Oct-19'
-END_DATE   = '10-Oct-19'
+START_DATE = '10-Oct-19'
+END_DATE   = '17-Oct-19'
