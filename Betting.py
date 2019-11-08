c545544f37beab6a63396a5dc644e94c733c0e1a
Will Mitchell <ChangedNameTo@users.noreply.github.com>
2019-11-08 09:41:09 -0500

    Initial betting processes commit

---
+++ Betting.py
@@ -0,0 +1,2 @@
+class Betting(object):
+    pass
--- Constants.py
+++ Constants.py
@@ -4,10 +4,15 @@ SCORE_URL    = 'http://prwolfe.bol.ucla.edu/cfootball/scores.htm'
 SCHEDULE_URL = 'http://prwolfe.bol.ucla.edu/cfootball/schedules.htm'
 
 YEAR      = 2019
-WEEK      = 10
+WEEK      = 11
 K_VALUE   = (24 - WEEK)
 
 SEED_DATE = '24-Aug-19'
 
-START_DATE = '31-Oct-19'
-END_DATE   = '06-Nov-19'
+START_DATE = '06-Nov-19'
+END_DATE   = '13-Nov-19'
+
+# Betting Info
+ODDS_URL = 'https://services.bovada.lv/services/sports/event/coupon/events/A/description/football/college-football?marketFilterId=def&preMatchOnly=true&lang=en'
+SEED_MONEY = 100
+BET_SIZE = SEED_MONEY / 20
