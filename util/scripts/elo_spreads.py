f7d3f10647a9ecbd21fe27050110c41993cdc79d
Will Mitchell <william.m95@gmail.com>
2019-11-20 10:00:07 -0500

    Moving things around to get better organized

--- util/scripts/elo_spreads.py
+++ util/scripts/elo_spreads.py
@@ -12,7 +12,8 @@ c.execute('''SELECT g.id AS game_id,
        round(ta.elo,2) AS away_elo,
        g.away_score AS away_score,
        abs(g.home_score - g.away_score) as observed_spread,
-       round((abs(th.elo - ta.elo) / 10),2) as predicted_spread
+       round((abs(th.elo - ta.elo) / 10),2) as predicted_spread,
+       round(abs(th.elo - ta.elo),2) as elo_diff
   FROM Games g
   JOIN Teams th ON g.home_id = th.id
   JOIN Teams ta ON g.away_id = ta.id;''')
@@ -21,6 +22,6 @@ rows = c.fetchall()
 
 with open(spread_csv_string, 'w') as spread_csv:
     writer = csv.writer(spread_csv, delimiter=',',quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(('Game ID','Home','Home Elo','Home Score','Away','Away Elo','Away Score','Obs. Spread','Pred. Spread'))
+    writer.writerow(('Game ID','Home','Home Elo','Home Score','Away','Away Elo','Away Score','Obs. Spread','Pred. Spread','Elo Diff.'))
 
     writer.writerows(rows)
