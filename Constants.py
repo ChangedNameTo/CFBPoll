ec459ad963425ddb5423e24598be979a4d9bf258
Will Mitchell <ChangedNameTo@users.noreply.github.com>
2020-09-09 15:23:21 -0400

    Initial Pandas Conversion

--- Constants.py
+++ Constants.py
@@ -1,21 +1,5 @@
 # Constants
-TEAM_LIST    = 'util/csvs/teams.txt'
-SCORE_URL    = 'http://prwolfe.bol.ucla.edu/cfootball/scores.htm'
-SCHEDULE_URL = 'http://prwolfe.bol.ucla.edu/cfootball/schedules.htm'
+YEAR      = 2020
+WEEK      = 1
 
-# YEAR      = 2018
-YEAR      = 2019
-WEEK      = 17
 K_VALUE   = 18
-
-SEED_DATE = '24-Aug-19'
-
-START_DATE = '14-Jan-20'
-END_DATE   = '14-Jan-20'
-
-# Betting Info
-# ODDS_URL = 'https://services.bovada.lv/services/sports/event/coupon/events/A/description/football/nfl?marketFilterId=def&preMatchOnly=true&lang=en'
-ODDS_URL = 'https://services.bovada.lv/services/sports/event/coupon/events/A/description/football/college-football?marketFilterId=def&preMatchOnly=true&lang=en'
-BASE_URL = 'https://www.bovada.lv/sports'
-SEED_MONEY = 100
-BET_SIZE = SEED_MONEY / 20
--- __main__.py
+++ __main__.py
@@ -1,28 +1,51 @@
-from Ranking import Ranking
+from pprint import pprint
 
-ranking = Ranking()
-ranking.generate_weeks()
-ranking.mean_reversion()
-ranking.run_poll()
+import pandas as pd
+import numpy as np
+import cfbd
+import os
 
-result = ranking.get_results()
-ranking.get_elo_array(result)
-ranking.set_mean_elo()
-ranking.set_median_elo()
-ranking.set_stdev_elo()
-ranking.set_variance_elo()
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
-ranking.generate_this_week()
+games = pd.DataFrame()
+teams = pd.DataFrame()
+plays = pd.DataFrame()
 
-ranking.get_sos_ranks()
-ranking.get_weakest_sos()
-ranking.get_hardest_sos()
+# This function is run once to collect the seed data for the model
+for year in range(2010, 2021):
+    # # Fetches all games, outputs the CSV's
+    # games_result = cfbd.GamesApi().get_games(year)
+    # games = pd.DataFrame.from_records([game.__dict__ for game in games_result])
+    # if not os.path.exists('data/{}'.format(year)):
+    #     os.makedirs('data/{}'.format(year))
+    # games.to_csv('data/{}/games.csv'.format(year))
 
-ranking.markdown_output()
+    # # Fetches all teams, outputs the CSV's
+    # teams_result = cfbd.TeamsApi().get_fbs_teams(year=year)
+    # teams = pd.DataFrame.from_records([team.__dict__ for team in teams_result])
+    # teams.to_csv('data/{}/teams.csv'.format(year))
 
-ranking.output_week_csv(result)
-ranking.conference_ranking()
-ranking.conference_output()
+    # Get the highest week
+    # highest_week = games['_week'].max() + 1
 
-# Prediction time
-ranking.parse_future()
+    # Pull all plays, concat them, then dump them into csvs
+    # year_plays = pd.DataFrame()
+    # for week in range(1, highest_week):
+    #     print(week)
+    #     plays_week_result = cfbd.PlaysApi().get_plays(year=year, week=week)
+    #     plays = pd.DataFrame.from_records([play.__dict__ for play in plays_week_result])
+    #     pd.concat([year_plays, plays])
+    
+    # year_plays.to_csv('data/{}/plays.csv'.format(year))
+    year_games = pd.read_csv('data/{}/games.csv'.format(year))
+    year_teams = pd.read_csv('data/{}/teams.csv'.format(year))
+    year_plays = pd.read_csv('data/{}/plays.csv'.format(year))
+
+    games = pd.concat([games, year_games])
+    teams = pd.concat([teams, year_teams])
+    plays = pd.concat([plays, year_plays])
+
+teams['elo'] = 1500
+print(teams.head())
