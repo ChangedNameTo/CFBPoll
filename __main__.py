2cbc3fe6e5ff5289929560852670dea173dcffca
Will Mitchell <william.m95@gmail.com>
2020-11-15 17:07:43 -0500

    Rebuild the Repo

--- __main__.py
+++ __main__.py
@@ -1,109 +1,62 @@
 import pandas as pd
 import numpy as np
 import cfbd
-import plotly.express as px
-import os
-import math
-import datetime as dt
-import sys
 
-from alive_progress import alive_bar
+data = data[
+    (data['home_points'] == data['home_points']) # filtering out future games
+    & (data['away_points'] == data['away_points'])
+    & (pd.notna(data['home_conference'])) # games with a non-FBS home team
+    & (pd.notna(data['away_conference'])) # games with a non-FBS away team
+]
 
-# Import my own functions
-sys.path.insert(1, './research')
-from Cycle import Cycle
-from stats_scraping import scrape_stats
+data['home_spread'] = np.where(data['neutral_site'] == True, data['home_points'] - data['away_points'], (data['home_points'] - data['away_points'] - 2.5))
+data['away_spread'] = -data['home_spread']
 
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('mode.chained_assignment', None) # Disable this if you have issues. This is to supress a warning in the SOS section of the Team code that is working as intended. 
 
-#############
-# Constants #
-#############
-# Runtime Constants - Use these to change when or how the poll runs
-YEAR      = 2020
-WEEK      = 8
-RUN_SCRAPER = True # Run this if the values in the csvs are out of date
+teams = pd.concat([
+    data[['home_team', 'home_spread', 'away_team']].rename(columns={'home_team': 'team', 'home_spread': 'spread', 'away_team': 'opponent'}),
+    data[['away_team', 'away_spread', 'home_team']].rename(columns={'away_team': 'team', 'away_spread': 'spread', 'home_team': 'opponent'})
+])
 
-# Value Constants - Tweak the poll outputs.
-# My default values are derived using the research contained in the /research directory.
-START_YEAR = 2010 
-TESTING = False # Set this to false if you care about poll output. 
-K_VALUE = 19
-HFA = 30 # Raw output is 3.04, converted to elo is 30.4. Rounds down to the whole number.
-FCS_ELO = 1204 # FCS Teams lose by, on average, 29.6 points. Converted to elo, 296. Average FBS team is 1500, so 1204 = 1500 - 296
-MEAN_REVERSION = 0.446153846153846 
+teams.head()
 
-# Dependent Variable
-D_V = 'k_value'
-LOW_D_V = 16
-HIGH_D_V = 24
-STEPS = 11
+teams['spread'] = np.where(teams['spread'] > 28, 28, teams['spread']) # cap the upper bound scoring margin at +28 points
+teams['spread'] = np.where(teams['spread'] < -28, -28, teams['spread']) # cap the lower bound scoring margin at -28 points
+teams.head()
 
-class Execution:
-    def build_options(self, iter_d_v=None):
-        self.options = {
-            'start_year':START_YEAR,
-            'testing':TESTING,
-            'k_value':K_VALUE,
-            'hfa':HFA,
-            'fcs_elo':FCS_ELO,
-            'mean_reversion':MEAN_REVERSION,
-            'end_year':YEAR,
-            'week':WEEK
-        }
+spreads = teams.groupby('team').spread.mean()
 
-        if TESTING:
-            self.options[D_V] = iter_d_v
 
-    def run(self):
-        # Should we run the scraper? This is controlled in Constants
-        if RUN_SCRAPER:
-            scrape_stats(WEEK)
+# create empty arrays
+terms = []
+solutions = []
 
-        # Is this a testing run or a real run? Testing runs are significantly longer
-        if TESTING:
-            # Set up the progressbar
-            with alive_bar((STEPS * (YEAR + 1 - START_YEAR))) as bar:
-                # Set up the output dataframe
-                outputs = pd.DataFrame(columns=['iter_year','perc_correct','dv_value'])
-                
-                for iter_d_v in np.linspace(LOW_D_V, HIGH_D_V, STEPS):
-                    testing_games = pd.DataFrame()
-                    testing_teams = pd.DataFrame()
-                    testing_plays = pd.DataFrame()
-                    testing_stats = pd.DataFrame()
-
-                    for iter_year in range(START_YEAR, YEAR + 1):
-                        self.build_options(iter_d_v)
-
-                        bar()
-
-                        # Run the cycle
-                        polling_cycle = Cycle(self.options, iter_year)
-                        results = polling_cycle.run()
-
-                        # Append the iterated dependent variable to our data set
-                        results.append(iter_d_v)
-                        results_series = pd.Series(results, index=outputs.columns)
-                        outputs = outputs.append(results_series, ignore_index=True)
-
-                # Dump the DV outputs to a CSV
-                outputs.to_csv('research/D_V.csv')            
-
-                # Print out the DV graph
-                fig = px.scatter(outputs, x='dv_value',y='perc_correct')
-                fig.write_html('research/dv_test.html')
+for team in spreads.keys():
+    row = []
+    # get a list of team opponents
+    opps = list(teams[teams['team'] == team]['opponent'])
+    
+    for opp in spreads.keys():
+        if opp == team:
+        	# coefficient for the team should be 1
+            row.append(1)
+        elif opp in opps:
+        	# coefficient for opponents should be 1 over the number of opponents
+            row.append(-1.0/len(opps))
         else:
-            with alive_bar(length=(YEAR + 1 - START_YEAR)) as bar:
-                for iter_year in range(START_YEAR, YEAR + 1):
-                    self.build_options()
+        	# teams not faced get a coefficient of 0
+            row.append(0)
+            
+    terms.append(row)
+    
+    # average game spread on the other side of the equation
+    solutions.append(spreads[team])
 
-                    bar()
-                    polling_cycle = Cycle(self.options, iter_year)
-                    output = polling_cycle.run()
+terms_df=pd.DataFrame(terms)
+terms_df.to_csv('test.csv')
+solutions = np.linalg.solve(np.array(terms), np.array(solutions))
 
-execution = Execution()
-execution.run()
+ratings = list(zip( spreads.keys(), solutions ))
+srs = pd.DataFrame(ratings, columns=['team', 'rating'])
+
+rankings = srs.sort_values('rating', ascending=False).reset_index()[['team', 'rating']]
--- research/stats_scraping.py
+++ research/stats_scraping.py
@@ -1,46 +1,48 @@
 import cfbd
 import pandas as pd
-import os
-import glob
-import json
 import requests
 
-from pandas.io.json import json_normalize
+def scrape_stats():
+    for year in range(2010, 2021):
+        conferences = [
+            'ACC',
+            'B12',
+            'B1G',
+            'SEC',
+            'PAC',
 
-def scrape_stats(end_week):
-    for year in range(2020, 2021):
-        # Fetches all games, outputs the CSV's
-        games_result = cfbd.GamesApi().get_games(year, season_type='both')
-        games = pd.DataFrame.from_records([game.__dict__ for game in games_result])
-        if not os.path.exists('data/{}'.format(year)):
-            os.makedirs('data/{}'.format(year))
+            'CUSA',
+            'MAC',
+            'MWC',
+            'AAC',
+            'SBC',
+
+            'Ind'
+        ]
+
+        for conference in conferences:
+            response = requests.get(
+                "https://api.collegefootballdata.com/games",
+                params={
+                    'year':year,
+                    'seasonType':'both',
+                    'conference':conference 
+                }
+            )
+
+            conference_games = pd.read_json(response.text)
+            conference_games.to_csv('data/{}/{}_games.csv'.format(year, conference))
+        
+        response = requests.get(
+            "https://api.collegefootballdata.com/games",
+            params={
+                'year':year,
+                'seasonType':'both',
+                'conference':conference 
+            }
+        )
+
+        games = pd.read_json(response.text)
         games.to_csv('data/{}/games.csv'.format(year))
 
-        # # Fetches all teams, outputs the CSV's
-        teams_result = cfbd.TeamsApi().get_fbs_teams(year=year)
-        teams = pd.DataFrame.from_records([team.__dict__ for team in teams_result])
-        teams.to_csv('data/{}/teams.csv'.format(year))
-
-        # # Pull all plays, concat them, then dump them into csvs
-        year_plays = pd.DataFrame()
-        for week in range(1, end_week + 1):
-            plays_week_result = cfbd.PlaysApi().get_plays(year=year, week=week)
-            plays = pd.DataFrame.from_records([play.__dict__ for play in plays_week_result])
-            year_plays = pd.concat([year_plays, plays])
-
-        year_plays.to_csv('data/{}/plays.csv'.format(year))
-
-        # # Pull all advanced stats, concat them, then dump them into csvs
-        year_stats = pd.DataFrame()
-        for week in range(1, end_week + 1):
-            stats_week_result = cfbd.StatsApi().get_team_season_stats(year=year, start_week=week, end_week=week+1)
-            stats = pd.DataFrame.from_records([stat.__dict__ for stat in stats_week_result])
-            year_stats = pd.concat([year_stats, stats])
-
-        year_stats.to_csv('data/{}/stats.csv'.format(year))
-
-        response = requests.get('https://api.collegefootballdata.com/records?year={}'.format(year))
-        json_records = response.json()
-        records = pd.json_normalize(json_records)
-
-        records.to_csv('data/{}/records.csv'.format(year))
+scrape_stats()
