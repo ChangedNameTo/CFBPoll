5fa0ec9fadce305b314a22baf629d7341f02f1bc
Will Mitchell <william.m95@gmail.com>
2020-11-21 11:43:50 -0500

    WIP

--- __main__.py
+++ __main__.py
@@ -1,10 +1,7 @@
 import pandas as pd
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
-from calculate_srs import calculate_srs
+game_data = pd.read_csv('data/master_games.csv')
 
-for year in range(2011,2020):
-    in_file_path = 'data/{}/games.csv'.format(year)
-    out_file_path = 'data/{}/processed_srs.csv'.format(year)
-
-    year_ranking = calculate_srs(in_file_path)
-    year_ranking.to_csv(out_file_path)
+games = game_data[['away_conference','away_id','away_points','away_team','conference_game','home_conference','home_id','home_points','home_team','neutral_site','season','season_type','start_date','week']]
