526ae4d82c841143ba882ad26d2d1d53c1e6bfba
Will Mitchell <william.m95@gmail.com>
2020-11-29 21:26:45 -0800

    Finalized model

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
-
+import statsmodels.api as sm
 import pandas as pd
 
 games_final = pd.read_csv('data/master_games_cleaned.csv')
@@ -12,10 +12,12 @@ games_final = games_final.fillna(0)
 X_set = games_final.loc[:, games_final.columns != 'did_home_win']
 y_set = games_final.loc[:, games_final.columns == 'did_home_win']
 
-X_train, X_test, y_train, y_test = train_test_split(X_set, y_set, test_size=0.3, random_state=0)
+X_scaled = preprocessing.scale(X_set)
 
-X_scaled = preprocessing.scale(X_train)
-y_scaled = preprocessing.scale(y_train)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_set, test_size=0.3, random_state=0)
 
 logreg = LogisticRegression()
-logreg.fit(X_scaled, y_scaled.ravel().astype(int))
+logreg.fit(X_train, y_train)
+
+y_pred = logreg.predict(X_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
